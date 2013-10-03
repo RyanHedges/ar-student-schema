e5b5a0fe1b7b3527a295c22372d067ebe793471c
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-10-02 20:42:06 -0700

    Third challenge done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,11 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :format => { :with => /.+@.+[.].{2,}/, :message => "Please enter valid email" }
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
@@ -10,3 +15,7 @@ class Student < ActiveRecord::Base
     ((Date.today - self.birthday).to_i / 365)
   end
 end
+
+
+
+# Email addresses must contain at least one @ character and one . character, with at least one character before the @, one character between the @ and first ., and at least two characters after the final ..
